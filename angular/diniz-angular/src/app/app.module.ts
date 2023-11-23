dd5e796b7059df7006c140a08e09896f7500800d
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-23 14:21:20 -0300

    creating toolbar with material

--- angular/diniz-angular/src/app/app.module.ts
+++ angular/diniz-angular/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
