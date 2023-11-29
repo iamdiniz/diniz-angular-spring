1b97523e7017b3a8492df529006ec2f7303b8ad8
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-29 12:42:12 -0300

    creating service

--- angular/diniz-angular/src/app/app.module.ts
+++ angular/diniz-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     MatToolbarModule,
     BrowserModule,
     AppRoutingModule,
--- angular/diniz-angular/src/app/courses/courses/courses.component.ts
+++ angular/diniz-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,16 +9,13 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course [] = [
-    { _id: '1', name: 'Angular 2', category: 'Angular' },
-  ];
-  
+  courses: Course [] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.courses = this.coursesService.list();
   }
 
 }
