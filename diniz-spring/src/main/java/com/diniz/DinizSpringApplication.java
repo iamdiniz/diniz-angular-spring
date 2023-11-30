f8bc1d5f34e4db133722b2155339c55be46954e8
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-30 15:02:47 -0300

    spring

---
+++ diniz-spring/src/main/java/com/diniz/DinizSpringApplication.java
@@ -0,0 +1,13 @@
+package com.diniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinizSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinizSpringApplication.class, args);
+	}
+
+}
