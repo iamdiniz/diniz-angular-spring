6e743cb458ec47e1d2de6601e77728daf4331202
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-30 13:19:31 -0300

    Creating icon and using pipe

--- angular/diniz-angular/src/app/shared/app-material/app-material.module.ts
+++ angular/diniz-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatIconModule,
     MatButtonModule,
     MatDialogModule,
     MatCardModule,
