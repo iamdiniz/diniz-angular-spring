634131310c7f4536bcc38535fd70ec0e40e28404
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-30 21:24:54 -0300

    spring project created

---
+++ spring/diniz-api/src/main/java/com/diniz/DinizApiApplication.java
@@ -0,0 +1,13 @@
+package com.diniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinizApiApplication.class, args);
+	}
+
+}
---
+++ spring/diniz-api/src/test/java/com/diniz/DinizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
