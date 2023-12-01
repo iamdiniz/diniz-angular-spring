bbebcf77925ac5f772464e42b793e68c951fdca1
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-12-01 15:40:49 -0300

    form to create

--- angular/diniz-angular/src/app/courses/courses.module.ts
+++ angular/diniz-angular/src/app/courses/courses.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { CourseFormComponent } from './course-form/course-form.component';
     CourseFormComponent
   ],
   imports: [
+    ReactiveFormsModule,
     MatProgressSpinnerModule,
     AppMaterialModule,
     CommonModule,
--- angular/diniz-angular/src/app/shared/app-material/app-material.module.ts
+++ angular/diniz-angular/src/app/shared/app-material/app-material.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
+    MatFormFieldModule,
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
+    MatInputModule
   ],
 })
 export class AppMaterialModule { }
