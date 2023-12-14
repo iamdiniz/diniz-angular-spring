fdeda93ab0a73fd748076c4df640f8178024fc84
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-12-14 16:43:29 -0300

    Basic 'PUT' method in Spring

--- spring/diniz-api/src/main/java/com/diniz/api/controller/CourseController.java
+++ spring/diniz-api/src/main/java/com/diniz/api/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,6 +41,18 @@ public class CourseController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Course create(@RequestBody Course newCourse) {
 		return courseRepository.save(newCourse);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course editCourse) {
+		return courseRepository.findById(id)
+				.map(entityCaseExist -> {
+					entityCaseExist.setName(editCourse.getName());
+					entityCaseExist.setCategory(editCourse.getCategory());
+					Course updatedCourse = courseRepository.save(entityCaseExist);
+					return ResponseEntity.ok().body(updatedCourse);
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
