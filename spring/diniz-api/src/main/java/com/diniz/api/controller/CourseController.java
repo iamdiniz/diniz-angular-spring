8f28e42398ea2cc89aa70ab70e796dc36409466d
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-12-14 17:03:34 -0300

    delete | spring

--- spring/diniz-api/src/main/java/com/diniz/api/controller/CourseController.java
+++ spring/diniz-api/src/main/java/com/diniz/api/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,16 @@ public class CourseController {
 					entityCaseExist.setCategory(editCourse.getCategory());
 					Course updatedCourse = courseRepository.save(entityCaseExist);
 					return ResponseEntity.ok().body(updatedCourse);
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		return courseRepository.findById(id)
+				.map(entityCaseExist -> {
+					courseRepository.deleteById(id);
+					return ResponseEntity.noContent().<Void>build();
 				})
 				.orElse(ResponseEntity.notFound().build());
 	}
