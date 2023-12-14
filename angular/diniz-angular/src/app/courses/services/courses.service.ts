312bb6a92a4eda16cb3f6731a3bcd33103409115
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-12-14 16:12:39 -0300

    saving form (put method)

--- angular/diniz-angular/src/app/courses/services/courses.service.ts
+++ angular/diniz-angular/src/app/courses/services/courses.service.ts
@@ -26,8 +26,19 @@ export class CoursesService {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
-  save(record: Partial<Course>) {
-    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  save(course: Partial<Course>) {
+    if (course._id) {
+      return this.update(course);
+    }
+    return this.create(course);
+  }
+
+  private create(course: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, course).pipe(first());
+  }
+
+  private update(course: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${course._id}`, course).pipe(first());
   }
 
 }
