41130680e425a56255ed1236ea582db52b527888
Guilherme Diniz <guilhermeplays03@gmail.com>
2024-02-07 14:04:42 -0300

    implementing formArray (lessons | frontend)

--- angular/diniz-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ angular/diniz-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,12 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
-import { CoursesService } from '../../services/courses.service';
 import { Course } from '../../model/course';
+import { Lesson } from '../../model/lesson';
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -14,29 +15,45 @@ import { Course } from '../../model/course';
 })
 export class CourseFormComponent implements OnInit {
 
-  form = this.formBuilder.group({
-    _id: [''],
-    name: ['', [Validators.required,
-       Validators.minLength(5),
-       Validators.maxLength(100)]],
-    category: ['', [Validators.required]]
-  });
-
   constructor(private formBuilder: NonNullableFormBuilder,
-     private service: CoursesService,
-     private snackBar: MatSnackBar,
-     private location: Location,
-     private route: ActivatedRoute) {
-   }
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location,
+    private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
     const course: Course = this.route.snapshot.data['course'];
-    this.form.setValue({
-      _id: course._id,
-      name: course.name,
-      category: course.category,
+    this.form = this.formBuilder.group({
+       _id: ['course._id'],
+       name: ['course.name', [Validators.required,
+       Validators.minLength(5),
+       Validators.maxLength(100)]],
+       category: ['course.category', [Validators.required]],
+       lessons: this.formBuilder.array(this.retrieveLessons(course))
+      });
+    console.log(this.form);
+    console.log(this.form.value);
+  }
+
+  form!: FormGroup;
+
+  private retrieveLessons(course: Course) {
+    const lessons = [];
+    if (course?.lessons) {
+      course.lessons.forEach(lesson => lessons.push(this.createLesson(lesson)));
+    } else {
+      lessons.push(this.createLesson());
+    }
+    return lessons;
+  }
+
+  private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}) {
+    return this.formBuilder.group({
+      id: [lesson.id],
+      name: [lesson.name],
+      youtubeUrl: [lesson.youtubeUrl]
     });
-    console.log(course);
   }
 
   onSubmit() {
