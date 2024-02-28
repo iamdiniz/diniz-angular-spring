aff58031c583db569da65d03625711c1df449b7a
Guilherme Diniz <guilhermeplays03@gmail.com>
2024-02-28 14:47:44 -0300

    implementing MySQL

--- spring/diniz-api/src/main/java/com/diniz/DinizApiApplication.java
+++ spring/diniz-api/src/main/java/com/diniz/DinizApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.diniz.domain.enums.Category;
 import com.diniz.domain.model.Course;
@@ -18,6 +19,7 @@ public class DinizApiApplication {
 	}
 	
 	@Bean
+	@Profile("dev")
 	CommandLineRunner initDataBase(CourseRepository courseRepository) {
 		return args -> {
 			courseRepository.deleteAll();
