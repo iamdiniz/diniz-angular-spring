750d64e4faa99faef9c30bbae9ef9fc855e5f1fd
Guilherme Diniz <guilhermeplays03@gmail.com>
2024-02-29 14:13:52 -0300

    migration to standalone components (using standalone api to make a boostrap on application)

--- angular/diniz-angular/src/app/app.component.ts
+++ angular/diniz-angular/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [MatToolbar, RouterOutlet]
 })
 export class AppComponent {
   title = 'diniz-angular';
--- angular/diniz-angular/src/main.ts
+++ angular/diniz-angular/src/main.ts
@@ -1,7 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(MatToolbarModule, BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
