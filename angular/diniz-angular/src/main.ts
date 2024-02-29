25a2d52d7caf128792ef5cb517a77c4510d50980
Guilherme Diniz <guilhermeplays03@gmail.com>
2024-02-29 14:33:04 -0300

    migration to standalone components (removing modules)

--- angular/diniz-angular/src/main.ts
+++ angular/diniz-angular/src/main.ts
@@ -1,20 +1,21 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(MatToolbarModule, BrowserModule, AppRoutingModule),
+        importProvidersFrom(MatToolbarModule, BrowserModule),
         provideHttpClient(withInterceptorsFromDi()),
-        provideAnimations()
+        provideAnimations(),
+        provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
     ]
 })
   .catch(err => console.error(err));
